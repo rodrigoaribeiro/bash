4ab8cec94d9a09a6088588e07160483be87f1982
Rodrigo Ribeiro <digao.listas@gmail.com>
2016-03-15 08:41:24 -0300

    primeiro commit scripts bash

---
+++ finddup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Encontrar linha duplicada em um arquivo texto
+# Rodrigo Ribeiro
+# https://github.com/rodrigoaribeiro
+# 15/03/2016 Versao 0.1
+
+
+arquivo="$1"
+for duplic in  $(cat $arquivo| sort | uniq -d); do     
+   egrep -n $duplic $arquivo
+done   
+
